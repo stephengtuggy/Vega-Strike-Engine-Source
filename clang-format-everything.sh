596c5b393d70f16b7290fca6c85de8220ab49a2c
Stephen G Tuggy <sgt@stephengtuggy.com>
2019-10-08 19:37:33 -0700

    refac(clang-format-everything.sh): Combine the multiple find commands into one

--- clang-format-everything.sh
+++ clang-format-everything.sh
@@ -1,13 +1,3 @@
 #! /usr/bin/env bash
 
-find . -type f -iname "*.h" -print0 | xargs -0 clang-format-8 -i
-clang-format-8 -i stringsort.cpp
-find launcher/ -type f -iname '*.cpp' -print0 | xargs -0 clang-format-8 -i
-find mission/ -type f -iname '*.cpp' -print0 | xargs -0 clang-format-8 -i
-find objconv/ -type f -iname '*.cpp' -print0 | xargs -0 clang-format-8 -i
-find saveinterface/ -type f -iname '*.cpp' -print0 | xargs -0 clang-format-8 -i
-find setup/ -type f -iname '*.cpp' -print0 | xargs -0 clang-format-8 -i
-find src/ -type f -iname '*.cpp' -print0 | xargs -0 clang-format-8 -i
-find tools/ -type f -iname '*.cpp' -print0 | xargs -0 clang-format-8 -i
-find vs_cubemap_gen/ -type f -iname '*.cpp' -print0 | xargs -0 clang-format-8 -i
-
+find . \( -not -path './boost/**' -not -path './.git/**' -not -name 'oldcollection*' \) \( -iname '*.h' -or -iname '*.cpp' \) -print0 | xargs -0 clang-format-8 -i
