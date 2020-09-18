5669016ddacf7851be0b048d3b082de3f7587573
Stephen G Tuggy <sgt@stephengtuggy.com>
2020-09-18 09:38:43 -0700

    script/bootstrap.ps1: Add opengl-registry; cmake-gui instead of cmake

--- script/bootstrap.ps1
+++ script/bootstrap.ps1
@@ -21,6 +21,7 @@ Set-Location vcpkg
 .\vcpkg.exe install libvorbis
 .\vcpkg.exe install openal-soft
 .\vcpkg.exe install opengl
+.\vcpkg.exe install opengl-registry
 .\vcpkg.exe install sdl1
 .\vcpkg.exe install zlib
 
@@ -29,4 +30,4 @@ Pop-Location
 mkdir build
 Set-Location build
 
-..\..\vcpkg\downloads\tools\cmake-3.17.2-windows\cmake-3.17.2-win32-x86\bin\cmake.exe -DUSE_PYTHON_3=ON ..\engine
+..\..\vcpkg\downloads\tools\cmake-3.17.2-windows\cmake-3.17.2-win32-x86\bin\cmake-gui.exe -DUSE_PYTHON_3=ON ..\engine
