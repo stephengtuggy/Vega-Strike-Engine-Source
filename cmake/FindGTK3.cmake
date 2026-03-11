146dfde04125685f7ceae8bf8e4da5e8f1318153
Stephen G Tuggy <sgt@stephengtuggy.com>
2026-03-11 16:17:15 -0700

    7x cmake/*.cmake: Add/update copyright headers

--- cmake/FindGTK3.cmake
+++ cmake/FindGTK3.cmake
@@ -1,3 +1,32 @@
+##
+# FindGTK3.cmake
+#
+# Vega Strike - Space Simulation, Combat and Trading
+# Copyright (C) 2001-2026 The Vega Strike Contributors:
+# Project creator: Daniel Horn
+# Original development team: As listed in the AUTHORS file
+# Current development team: Roy Falk, Benjamen R. Meyer, Stephen G. Tuggy
+#
+# https://github.com/vegastrike/Vega-Strike-Engine-Source
+#
+# This file is part of Vega Strike.
+#
+# Vega Strike is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Vega Strike is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Vega Strike.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+
+##
 # - FindGTK3.cmake
 # This module can find the GTK3 widget libraries and several of its other
 # optional components like gtkmm, glade, and glademm.
@@ -31,7 +60,7 @@
 #   GTK3_ADDITIONAL_SUFFIXES - Allows defining additional directories to
 #                              search for include files
 #
-#=================
+# =================
 # Example Usage:
 #
 #   Call FIND_PACKAGE() once, here are some examples to pick from:
@@ -45,6 +74,7 @@
 #      TARGET_LINK_LIBRARIES(mygui ${GTK3_LIBRARIES})
 #   ENDIF()
 #
+
 
 #=============================================================================
 # Copyright 2009 Kitware, Inc.
