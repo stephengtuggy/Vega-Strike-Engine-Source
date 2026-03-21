6317367af0b2221b87c2eb02665d1f599d73d15d
Stephen G Tuggy <sgt@stephengtuggy.com>
2026-03-21 12:38:30 -0700

    opvector3.cpp: Update LGPL version to 3

--- libraries/collide2/csgeom2/opvector3.cpp
+++ libraries/collide2/csgeom2/opvector3.cpp
@@ -1,24 +1,26 @@
 /*
-    opvector3.cpp
-
-    Copyright (C) 1998,1999,2000 by Jorrit Tyberghein
-    Largely rewritten by Ivan Avramovic <ivan@avramovic.com>
-    Copyright (C) 2022-2025 Benjamen R. Meyer and Stephen G. Tuggy
-
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
-
-    You should have received a copy of the GNU Library General Public
-    License along with this library; if not, write to the Free
-    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-*/
+ * opvector3.cpp
+ *
+ * Vega Strike - Space Simulation, Combat and Trading
+ * Copyright (C) 1998,1999,2000 by Jorrit Tyberghein
+ * Largely rewritten by Ivan Avramovic <ivan@avramovic.com>
+ * Copyright (C) 2022-2025 Benjamen R. Meyer and Stephen G. Tuggy
+ * Copyright (C) 2026 The Vega Strike Contributors
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see
+ * <https://www.gnu.org/licenses/>.
+ */
 
 
 #include "collide2/opcodesysdef.h"
