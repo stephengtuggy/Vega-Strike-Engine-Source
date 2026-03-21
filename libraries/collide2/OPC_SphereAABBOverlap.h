43faae938e1194b39ec0b561d96673b0ea42cb8b
Stephen G Tuggy <sgt@stephengtuggy.com>
2026-03-20 22:05:39 -0700

    Apply copyright header updates to 26 files under libraries/collide2

--- libraries/collide2/OPC_SphereAABBOverlap.h
+++ libraries/collide2/OPC_SphereAABBOverlap.h
@@ -1,10 +1,12 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/**
+/*
  * OPC_SphereAABBOverlap.h
  *
+ * Vega Strike - Space Simulation, Combat and Trading
  * Copyright (C) 2001 Pierre Terdiman
  * Copyright (C) 2021, 2022, 2025 Stephen G. Tuggy
  * Copyright (C) 2023 Benjamen R. Meyer
+ * Copyright (C) 2026 The Vega Strike Contributors
  *
  * This file is part of OPCODE - Optimized Collision Detection
  * (http://www.codercorner.com/Opcode.htm) and has been
