e46b7d5eff15da602b8732e2beeb073018c6fdcf
Stephen G Tuggy <sgt@stephengtuggy.com>
2026-03-27 04:38:52 -0700

    140x engine/src/gfx/**/*.h, *.cpp: Update copyright headers to 2026

--- engine/src/gfx/cockpit_gfx_utils.cpp
+++ engine/src/gfx/cockpit_gfx_utils.cpp
@@ -3,8 +3,9 @@
 /*
  * cockpit_gfx_utils.cpp
  *
+ * Vega Strike - Space Simulation, Combat and Trading
  * Copyright (C) 2020-2025 Daniel Horn, Roy Falk, Stephen G. Tuggy, and
- * other Vega Strike contributors
+ * Copyright (C) 2026 The Vega Strike Contributors
  *
  * https://github.com/vegastrike/Vega-Strike-Engine-Source
  *
--- engine/src/gfx/jpeg_memory.cpp
+++ engine/src/gfx/jpeg_memory.cpp
@@ -2,12 +2,10 @@
  * jpeg_memory.cpp
  *
  * Vega Strike - Space Simulation, Combat and Trading
- * Copyright (C) 2001-2025 The Vega Strike Contributors:
+ * Copyright (C) 2001-2026 The Vega Strike Contributors:
  * Project creator: Daniel Horn
- * Original development team: As listed in the AUTHORS file. Specifically:
- * surfdargent, dan_w
+ * Original development team: As listed in the AUTHORS file. Specifically: surfdargent, dan_w
  * Current development team: Roy Falk, Benjamen R. Meyer, Stephen G. Tuggy
- *
  *
  * https://github.com/vegastrike/Vega-Strike-Engine-Source
  *
